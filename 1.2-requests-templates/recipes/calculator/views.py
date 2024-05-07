26fc6a3da6c0a43293bfebdcf3feef289cfc17d2
Nikolay Mazur <nmazur79@gmail.com>
2024-05-07 19:13:17 +0300

    Files created 07May2024

---
+++ 1.2-requests-templates/recipes/calculator/views.py
@@ -0,0 +1,43 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+
+DATA = {
+    'omlet': {
+        'яйца, шт': 2,
+        'молоко, л': 0.1,
+        'соль, ч.л.': 0.5,
+    },
+    'pasta': {
+        'макароны, г': 0.3,
+        'сыр, г': 0.05,
+    },
+    'buter': {
+        'хлеб, ломтик': 1,
+        'колбаса, ломтик': 1,
+        'сыр, ломтик': 1,
+        'помидор, ломтик': 1,
+    },
+    # можете добавить свои рецепты ;)
+}
+
+
+# Напишите ваш обработчик. Используйте DATA как источник данных
+# Результат - render(request, 'calculator/index.html', context)
+# В качестве контекста должен быть передан словарь с рецептом:
+# context = {
+#   'recipe': {
+#     'ингредиент1': количество1,
+#     'ингредиент2': количество2,
+#   }
+# }
+
+def select_recipe(request):
+    qty = int(request.GET.get("servings", 1))
+    menu = {}
+    for key, value in DATA.items():
+        menu[key] = {}
+        for inner_key, inner_value in value.items():
+            menu[key][inner_key] = inner_value * qty
+
+    for recipe in menu.keys():
+        return HttpResponse(render(request, f'{recipe}/index.html', context=menu))
---
+++ readme.md
@@ -0,0 +1,6 @@
+# [Домашнее задание "Рецепты"](https://github.com/netology-code/dj-homeworks/tree/video/1.2-requests-templates/recipes)
+
+### Измененя внесены:
+✅ [apps/views.py](https://github.com/Nikolay08041979/django_project-1/blob/master/1.1-first-project/first_project/app/views.py)
+
+✅ [first_project/urls.py](https://github.com/Nikolay08041979/django_project-1/blob/master/1.1-first-project/first_project/first_project/urls.py)
