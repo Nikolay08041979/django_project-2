87a435dfc5adc5a29fd02df375704c9232a8957f
Nikolay Mazur <nmazur79@gmail.com>
2024-05-08 15:18:37 +0300

    Files modified 08May2024

--- 1.2-requests-templates/pagination/stations/views.py
+++ 1.2-requests-templates/pagination/stations/views.py
@@ -1,17 +1,26 @@
+from csv import DictReader
+
+from django.core.paginator import Paginator
 from django.shortcuts import render, redirect
 from django.urls import reverse
-
 
 def index(request):
     return redirect(reverse('bus_stations'))
 
 
 def bus_stations(request):
+    with open('data-398-2018-08-30.csv', newline='', encoding='utf8') as f:
+        busstations = []
+        for row in DictReader(f):
+            busstations.append(row)
+        page_number = int(request.GET.get('page', 1))
+        paginator = Paginator(busstations, 10)
+        page = paginator.get_page(page_number)
+        context = {
+            'bus_stations': paginator,
+            'page': page,
+        }
     # получите текущую страницу и передайте ее в контекст
     # также передайте в контекст список станций на странице
 
-    context = {
-    #     'bus_stations': ...,
-    #     'page': ...,
-    }
     return render(request, 'stations/index.html', context)
--- readme.md
+++ readme.md
@@ -1,6 +1,14 @@
-# [Домашнее задание "Рецепты"](https://github.com/netology-code/dj-homeworks/tree/video/1.2-requests-templates/recipes)
+# Домашнее задание №2
+
+## ["Рецепты"](https://github.com/Nikolay08041979/django_project-2/tree/master/1.2-requests-templates/recipes)
 
 ### Измененя внесены:
 ✅ [calculator/views.py](https://github.com/Nikolay08041979/django_project-2/blob/master/1.2-requests-templates/recipes/calculator/views.py)
 
 ✅ [recipes/urls.py](https://github.com/Nikolay08041979/django_project-2/blob/master/1.2-requests-templates/recipes/recipes/urls.py)
+
+
+## [Пагинация](https://github.com/Nikolay08041979/django_project-2/tree/master/1.2-requests-templates/pagination)
+
+### Измененя внесены:
+✅ [stations/views.py](https://github.com/Nikolay08041979/django_project-2/blob/master/1.2-requests-templates/recipes/calculator/views.py)
